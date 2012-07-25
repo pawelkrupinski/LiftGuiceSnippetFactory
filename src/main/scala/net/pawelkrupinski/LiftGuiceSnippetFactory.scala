99d5a46e5b2dca5bfa0f20393cda52295aa115a6
Paweł Krupinski <pawel@krupinski.com>
2012-07-25 23:10:03 +0100

    Cleanup

--- src/main/scala/net/pawelkrupinski/LiftGuiceSnippetFactory.scala
+++ src/main/scala/net/pawelkrupinski/LiftGuiceSnippetFactory.scala
@@ -7,25 +7,25 @@ import net.liftweb.util.Helpers._
 import java.lang.reflect.Method
 
 class LiftGuiceSnippetFactory(val injector: Injector, val liftRules: LiftRules) {
+  private val defaultMethod: String = "render"
+
   def registerWithLift() {
     liftRules.snippets.append(snippetFunction)
   }
 
   private def snippetFunction: PartialFunction[scala.List[String], (NodeSeq) => NodeSeq] = {
     case className :: method :: Nil if (findSnippetMethod(className, method).isDefined) => fetchSnippet(className, method)
-    case className :: Nil if (findSnippetMethod(className).isDefined) => fetchSnippet(className, "render")
+    case className :: Nil if (findSnippetMethod(className).isDefined) => fetchSnippet(className, defaultMethod)
   }
 
-  private def findSnippetMethod(className: String, method: String = "render") =
+  private def findSnippetMethod(className: String, method: String = defaultMethod) =
     findClass(className, liftRules.buildPackage("snippet"))
       .flatMap(_.getMethods().find(_.getName == method))
 
-  private def fetchSnippet(className: String, method: String): (NodeSeq) => NodeSeq =
-    findSnippetMethod(className, method).map(method =>
+  private def fetchSnippet(className: String, methodName: String): (NodeSeq) => NodeSeq =
+    findSnippetMethod(className, methodName).map(method =>
       callMethodOrApplyFunction(instantiate(method.getDeclaringClass.asInstanceOf[Class[AnyRef]]), method) _
     ).openTheBox
-
-  private def instantiate[T](theClass: Class[T]): T = injector.getInstance(theClass)
 
   private def callMethodOrApplyFunction(instance: AnyRef, method: Method)(nodes: NodeSeq): NodeSeq = {
     if (classOf[NodeSeq].isAssignableFrom(method.getReturnType)) {
@@ -34,4 +34,6 @@ class LiftGuiceSnippetFactory(val injector: Injector, val liftRules: LiftRules) 
       method.invoke(instance).asInstanceOf[NodeSeq => NodeSeq].apply(nodes)
     }
   }
+
+  private def instantiate[T](theClass: Class[T]): T = injector.getInstance(theClass)
 }
